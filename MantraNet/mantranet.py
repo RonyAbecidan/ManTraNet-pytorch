8b8732a946062c1846f56e9ff8358cfc6b0ac00a
Rony Abecidan <44161052+RonyAbecidan@users.noreply.github.com>
2022-09-27 15:54:52 +0200

    [Fix] : Fix Issue #12

--- MantraNet/mantranet.py
+++ MantraNet/mantranet.py
@@ -467,11 +467,13 @@ class IMTFE(nn.Module):
         self.init_conv = nn.Conv2d(in_channel, 4, 5, 1, padding=0, bias=False)
 
         self.BayarConv2D = nn.Conv2d(in_channel, 3, 5, 1, padding=0, bias=False)
-        self.bayar_mask = (torch.tensor(np.ones(shape=(5, 5)))).to(self.device)
+        self.bayar_mask = torch.tensor(np.ones(shape=(5, 5)))
         self.bayar_mask[2, 2] = 0
+        self.bayar_mask.to(self.device)
 
-        self.bayar_final = (torch.tensor(np.zeros((5, 5)))).to(self.device)
+        self.bayar_final = torch.tensor(np.zeros((5, 5)))
         self.bayar_final[2, 2] = -1
+        self.bayar_final.to(self.device)
 
         self.SRMConv2D = nn.Conv2d(in_channel, 9, 5, 1, padding=0, bias=False)
         self.SRMConv2D.weight.data=torch.load('IMTFEv4.pt')['SRMConv2D.weight']
